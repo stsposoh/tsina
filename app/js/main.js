489e0c49c5539c8232ef5e58e5e2a451dcd2f70e
Posokhov Stas <stas.p.solardigital@gmail.com>
2020-06-14 16:52:16 +0300

    Listing page added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
+$(window).on('resize', function() {
+  if ($(window).width() < 768) {
+    $('.listing-grid__cards').removeClass('--horizontal-cards');
+    $('.toggle-view__btn[data-listing-view="list"]').removeClass('--active');
+    $('.toggle-view__btn[data-listing-view="tile"]').addClass('--active');
+  }
+});
+
+
 $('.js-tabs').on('click', '[data-tab]', function () {
   let tabId = $(this).data('tab');
   let parentEl = $(this).parents('.js-tabs');
@@ -10,3 +19,16 @@ $('.js-tabs').on('click', '[data-tab]', function () {
   parentEl.find(`[data-tab-content="${tabId}"]`).addClass('--active');
 });
 
+$('.toggle-view__btn').on('click', function () {
+  const parent = $(this).parents('.toggle-view');
+  const listing = $('.listing-grid__cards');
+
+  parent.find('.toggle-view__btn').removeClass('--active');
+  $(this).addClass('--active');
+
+  if($(this).data('listing-view') === 'list') {
+    listing.addClass('--horizontal-cards');
+  } else {
+    listing.removeClass('--horizontal-cards');
+  }
+});
